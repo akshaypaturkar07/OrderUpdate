1f50d0ae9e789011ae96a1eeba1341ba5733ef45
Akshay Paturkar <akshay.paturkar@jda.com>
2020-06-26 22:06:46 +0530

    Added spring cloud dependencies

--- src/main/java/com/mydomain/OrderDetails/OrderDetailsApplication.java
+++ src/main/java/com/mydomain/OrderDetails/OrderDetailsApplication.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,9 +19,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableWebSecurity
+@EnableEurekaClient
 public class OrderDetailsApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
@@ -74,5 +78,11 @@ public class OrderDetailsApplication extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout() // Metodo get pues he desabilitado CSRF
                 .permitAll();
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 }
